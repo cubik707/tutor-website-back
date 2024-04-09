b185dacb2c49b6d8cede203c5e153d86e3f84b04
Vladislava <stdemidovez@gmail.com>
2024-04-09 23:41:01 +0300

    install nodemon

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-let a;
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req,res) => {
+    res.send('HelloWorld!');
+});
+
+app.listen(4444, (err) => {
+    if (err) {
+        return console.log(err);
+    }
+    console.log('Server OK')
+})
