a60d5fdb0b64b4b4ef5b8055c1d3e00fa0c13173
Vladislava <stdemidovez@gmail.com>
2024-04-12 00:45:29 +0300

    complete register

--- index.js
+++ index.js
@@ -21,28 +21,47 @@ app.get('/', (req, res) => {
 });
 
 app.post('/auth/register', registerValidation, async (req, res) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json(errors.array());
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json(errors.array());
+        }
+
+        //Шифрование пароля
+        const password = req.body.password;
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(password, salt);
+
+        const doc = new UserModel({
+            email: req.body.email,
+            fullName: req.body.fullName,
+            avatarUrl: req.body.avatarUrl,
+            passwordHash: hash,
+        });
+
+        const user = await doc.save(); // Сохранение в БД
+
+        const token = jwt.sign({
+                _id: user._id,
+            },
+            'secret123',
+            {
+                expiresIn: '30d', //Срок жизни токена
+            });
+
+        const {passwordHash, ... userData} = user._doc;
+
+        res.json({
+            ...userData,
+            token,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Не удалось зарегестрироваться',
+        });
     }
 
-    //Шифрование пароля
-    const password = req.body.password;
-    const salt = await bcrypt.genSalt(10);
-    const passwordHash = await bcrypt.hash(password, salt);
-
-    const doc = new UserModel({
-        email: req.body.email,
-        fullName: req.body.fullName,
-        avatarUrl: req.body.avatarUrl,
-        passwordHash,
-    });
-
-    const user = await doc.save(); // C
-
-    res.json({
-        success: true,
-    });
 
     // console.log(req.body);
     //
