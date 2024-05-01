602f0ad7c52d947419ebcfe8e3174fa94b591dbe
Vladislava <stdemidovez@gmail.com>
2024-05-01 14:34:07 +0300

    connect multer

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express'; //Подключение express
 import mongoose from "mongoose"; //Подключение mongoose
+import multer from "multer" // Для загрузки картинок на сервер
 
 import {loginValidation, registerValidation} from './validations/authValidation.js'
 import checkAuth from './utils/checkAuth.js'
@@ -18,16 +19,30 @@ mongoose.connect('mongodb://localhost:27017')
 
 const app = express(); //Создание express приложения
 
+const storage = multer.diskStorage({
+    destination: (_, __, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (_, file, cb) => {
+        cb(null, file.originalname)
+    },
+})
+
+const upload = multer({storage});
+
 app.use(express.json()); //Чтение файлов JSON с помощью express
 
-// app.get('/', (req, res) => {
-//     res.send('HelloWorld');
-// });
 
 //Авторизация и регистрация
 app.post('/auth/register',registerValidation , UserController.register);
 app.post('/auth/login', loginValidation, UserController.login);
 app.get('/auth/me', checkAuth, UserController.getMe);
+
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+    res.json({
+        url: `/upload/${req.file.originalname}`,
+    })
+})
 
 //Работа с репетиторами
 app.post('/auth/tutor', checkAuth, tutorCreateValidation, TutorController.createTutor);
